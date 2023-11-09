6da4136e1530c9f548ae1f4457391fbbf2c77e35
donramilio <ramon-spb@mail.ru>
2023-11-09 23:04:40 +0300

    init project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,49 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Test");
+    printThreeWords();
+    checkSumSign();
+    printColor();
+    compareNumbers();
+  }
+
+  public static void printThreeWords() {
+    System.out.println("Orange");
+    System.out.println("Banana");
+    System.out.println("Apple");
+  }
+
+  public static void checkSumSign() {
+    int a = 1;
+    int b = 2;
+    if (a + b >= 0) {
+      System.out.println("Сумма положительная");
+    }
+    if (a + b < 0) {
+      System.out.println("Сумма отрицательная");
+    }
+  }
+
+  public static void printColor() {
+    int value = 100;
+    if (value <= 0) {
+      System.out.println("Красный");
+    }
+    if (value > 0 && value <= 100) {
+      System.out.println("Жёлтый");
+    }
+    if (value > 100) {
+      System.out.println("Зелёный");
+    }
+  }
+
+  public static void compareNumbers() {
+    int a = 65;
+    int b = 77;
+    if (a >= b) {
+      System.out.println("a >= b");
+    }
+    else System.out.println("a < b");
+
+
   }
 }
