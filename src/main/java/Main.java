742dc778bf6e6dac8782a51fd37a1fb7b52576fa
donramilio <ramon-spb@mail.ru>
2023-11-15 14:56:12 +0300

    init project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,64 @@
 public class Main {
+  public static void main(String[] args) {
+    System.out.println(isBetween10And20(10, -2));
+    System.out.println(isBetween10And20(12, 5));
+    System.out.println(isBetween10And20(20, 1));
+
+    isPositiveOrNegative(-1);
+    isPositiveOrNegative(0);
+    isPositiveOrNegative(1);
+
+    System.out.println(isNegativOrPositive(-1));
+    System.out.println(isNegativOrPositive(1));
+    System.out.println(isNegativOrPositive(0));
+
+    printLine("Високосный", 2);
+
+    System.out.println(doleapYear(2024));
+  }
+  /** Задание.1
+   * Написать метод, принимающий на вход два целых числа и проверяющий,
+   * что их сумма лежит в пределах от 10 до 20 (включительно),
+   * если да – вернуть true, в противном случае – false.
+   */
+  static boolean isBetween10And20(int a, int b) {
+    return a + b >=10 && a + b <=20;
+  }
+
+  /** Задание.2
+   * Написать метод, которому в качестве параметра передается целое число,
+   * метод должен напечатать в консоль, положительное ли число передали
+   * или отрицательное. Замечание: ноль считаем положительным числом.
+   */
+  static void isPositiveOrNegative(int a) {
+    System.out.println(a >= 0? "Положительное" : "Отпицательное");
+  }
+
+  /** Задание.3
+   * 3. Написать метод, которому в качестве параметра передается целое число.
+   * Метод должен вернуть true, если число отрицательное,
+   * и вернуть false если положительное.
+   */
+  static boolean isNegativOrPositive(int a) {
+    return a < 0;
+  }
+
+  /** Задание.4
+   * Написать метод, которому в качестве аргументов передается строка
+   * и число, метод должен отпечатать в консоль указанную строку,
+   * указанное количество раз;
+   */
+  static void printLine(String line, int n) {
+    for (int i = 0; i < n; i++)
+      System.out.println(line);
+  }
+  /** Задание.5
+   * Написать метод, который определяет, является ли год високосным,
+   * и возвращает boolean (високосный - true, не високосный - false).
+   * Каждый 4-й год является високосным, кроме каждого 100-го,
+   * при этом каждый 400-й – високосный.
+   */
+  static boolean doleapYear(int year) {
+    return year % 100 != 0 && year % 4 == 0 || year % 400 ==0;
+  }
 }
